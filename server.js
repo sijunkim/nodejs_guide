c682be8a7c37cca2bf420c66ca5d80f4ded486e3
kimsijun <papaya9349@naver.com>
2022-04-22 20:52:30 +0900

    managing cluster

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 3000;
+// const http = require("http");
+// const fs = require("fs");
 
-// const indexRouter = require("./router/indexRouter");
-
-// app.get("/", (req, res) => {
-//   res.json({
-//     success: true,
+// http.createServer((req, res) => {
+//   fs.readFile("./html/server.html", (err, data) => {
+//     if (err) {
+//       throw err;
+//     }
+//     res.end(data);
 //   });
-// });
-// app.use("/index", indexRouter);
-
-// const server = app.listen(port, () => {
-//   console.log(`server on ${port}`);
+// }).listen(8081, () => {
+//   console.log("8081번 포트에서 서버 대기 중입니다!");
 // });
 
-const http = require("http");
-const fs = require("fs");
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
 
-http.createServer((req, res) => {
-  fs.readFile("./html/server.html", (err, data) => {
-    if (err) {
-      throw err;
-    }
-    res.end(data);
+if (cluster.isMaster) {
+  console.log(`마스터 프로세스 아이디 : ${process.pid}`);
+
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`${worker.process.pid}번 워커가 종료되었습니다.`);
   });
-}).listen(8081, () => {
-  console.log("8081번 포트에서 서버 대기 중입니다!");
-});
+} else {
+  // 워커들이 포트에서 대기
+  http.createServer((req, res) => {
+    res.write('<h1>Hello Node!</h1>');
+    res.end('<p>Hello Cluster!</p>');
+  }).listen(8085); console.log(`${process.pid}번 워커 실행`);
+}
